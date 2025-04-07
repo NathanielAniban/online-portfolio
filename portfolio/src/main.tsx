2f1939a1e760bcbf0426c717b4806dc0493a7e06
NathanielAniban <114843841+NathanielAniban@users.noreply.github.com>
2025-04-07 04:31:01 +0000

    push

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { inject } from "@vercel/analytics";
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
-injectSpeedInsights();
 inject();
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
