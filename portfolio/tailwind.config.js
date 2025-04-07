e82e925cd9e91a25048e3e1b72360c6e175bb1a9
NathanielAniban <114843841+NathanielAniban@users.noreply.github.com>
2025-04-07 05:16:30 +0000

    push

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx}', // Add tsx and ts extensions here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
