3c1fedec1ed844bd2f57de73ca26500ac16cae2e
NathanielAniban <114843841+NathanielAniban@users.noreply.github.com>
2025-04-07 05:00:20 +0000

    commit

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' https://cdn.tailwindcss.com;",
+    }
+  }
 })
